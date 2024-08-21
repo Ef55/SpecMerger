364a84a717641270f3fc14b9b09bb0cdb66bdd64
MartinWho2 <martin.crettol@gmail.com>
2024-08-21 11:54:58 +0200

    Add README

--- spec_merger/generic_main.py
+++ spec_merger/generic_main.py
@@ -21,16 +21,13 @@ def main(open_in_browser: bool):
 
     a = Aligner()
     result = a.align(coq_parsed_page.entries, ecma_parsed_page_v14.entries)
-    with open("text_output.txt","w+") as f:
-        f.write(result.to_text(0))
-    exit(0)
     renderer = HTMLRenderer(result)
     errors = renderer.get_errors()
     if errors.error_count == 0:
         print("The documents correspond !")
     else:
         print("ERRORS DETECTED")
-    render = renderer.render()
+    render = renderer.render(path_to_template="spec_merger")
     with open("generic_result.html", "w+") as f:
         f.write(render)
     if open_in_browser:
--- spec_merger/special_comparator.py
+++ spec_merger/special_comparator.py
@@ -12,6 +12,4 @@ class SpecialComparator:
             case (t1, t2) if type(t1) is not type(t2):
                 return Misalignment((elem1.position, elem2.position), elem1, elem2, ReportErrorType.MISMATCHED_TYPES)
             case _:
-                print(
-                    f"[WARNING!!!!!] Function is same type but not registered in SpecialComparator???? \n{elem1}\n VS \n{elem2}")
                 return Misalignment((elem1.position, elem2.position), elem1, elem2, ReportErrorType.UNKNOWN_ERROR)
